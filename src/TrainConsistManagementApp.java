c114ba530e03ced288177dd3424f612589cc2f32
Dheirya <108139056+DheiryaMehta10@users.noreply.github.com>
2026-04-07 14:31:30 +0530

    Update TrainConsistManagementApp.java

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,44 +1,31 @@
 public class Train_App {
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
+    public static void main(String[] args) {
+        System.out.println("============================================");
+        System.out.println(" UC20 - Exception Handling During Search ");
+        System.out.println("============================================\n");
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        String[] bogieIds = {};
 
-                if (capacities[j] > capacities[j + 1]) {
+        String searchId = "BG101";
 
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
+        if (bogieIds.length == 0) {
+            throw new IllegalStateException("No bogies available in train. Cannot perform search.");
+        }
+
+        boolean found = false;
+        for (String id : bogieIds) {
+            if (id.equals(searchId)) {
+                found = true;
+                break;
             }
         }
-    }
 
-    // Method to print array
-    public static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
+        if (found) {
+            System.out.println("Bogie " + searchId + " found in train.");
+        } else {
+            System.out.println("Bogie " + searchId + " not found in train.");
         }
-        System.out.println();
-    }
 
-    public static void main(String[] args) {
-
-        System.out.println("===============================================");
-        System.out.println(" UC16 - Manual Sorting using Bubble Sort ");
-        System.out.println("===============================================\n");
-
-        int[] capacities = {72, 56, 24, 70, 60};
-
-        System.out.println("Original Capacities:");
-        printArray(capacities);
-
-        bubbleSort(capacities);
-
-        System.out.println("\nSorted Capacities (Ascending):");
-        printArray(capacities);
-
-        System.out.println("\nUC16 sorting completed...");
+        System.out.println("\nUC20 execution completed...");
     }
 }
